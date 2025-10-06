a009dd31acba868cdd23f569874ad3aa4ef1a1fe
S Fauzi <hello@sfauzi.dev>
2025-10-06 10:23:29 +0700

    feat: configuaration database

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"goweb-project/config"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	config.ConnectDB()
+
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8081", nil)
 }
