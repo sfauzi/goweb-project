5a1243c5b78f1030fd67e1b41153c349009e79cc
S Fauzi <hello@sfauzi.dev>
2025-10-06 12:02:29 +0700

    feat: add function edit and update

--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -5,6 +5,7 @@ import (
 	"goweb-project/models/categorymodel"
 	"html/template"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -50,9 +51,53 @@ func Add(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/categories", http.StatusSeeOther)
 	}
 }
+
 func Edit(w http.ResponseWriter, r *http.Request) {
 
+	if r.Method == "GET" {
+
+		temp, err := template.ParseFiles("views/category/edit.html")
+		if err != nil {
+			panic(err)
+		}
+
+		idString := r.URL.Query().Get("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			panic(err)
+		}
+
+		category := categorymodel.Detail(id)
+		data := map[string]any{
+			"category": category,
+		}
+
+		temp.Execute(w, data)
+	}
+
+	if r.Method == "POST" {
+		var category entities.Category
+
+		idString := r.FormValue("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			panic(err)
+		}
+
+		category.Name = r.FormValue("name")
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Update(id, category); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+			return
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+
+	}
+
 }
+
 func Delete(w http.ResponseWriter, r *http.Request) {
 
 }
--- models/categorymodel/categorymodel.go
+++ models/categorymodel/categorymodel.go
@@ -47,3 +47,29 @@ func Create(category entities.Category) bool {
 	return lastInserId > 0
 
 }
+
+func Detail(id int) entities.Category {
+
+	row := config.DB.QueryRow(`SELECT id, name FROM categories WHERE id = ?`, id)
+
+	var category entities.Category
+	if err := row.Scan(&category.Id, &category.Name); err != nil {
+		panic(err.Error())
+	}
+
+	return category
+}
+
+func Update(id int, category entities.Category) bool {
+	query, err := config.DB.Exec(`UPDATE categories SET name = ?, updated_at = ? WHERE id = ?`, category.Name, category.UpdatedAt, id)
+
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := query.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+	return result > 0
+}
